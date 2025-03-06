4868c0418daa00cbe6bd7cdc21fb3c25bec9f9d3
azizaalijonova <aziza.alijonova9@gmail.com>
2025-03-06 17:58:16 +0500

    changes

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const services = [
-    { name: "NEUROSCIENCES", image: "../images/services.svg" },
-    { name: "CARDIOLOGY", image: "../images/services.svg" },
-    { name: "ORTHOPEDICS", image: "../images/services.svg" },
-    // { name: "DERMATOLOGY", image: "../images/services.svg" },
-    // { name: "PEDIATRICS", image: "../images/services.svg" },
-    // { name: "GYNECOLOGY", image: "../images/services.svg" },
-    // { name: "UROLOGY", image: "../images/services.svg" },
-    // { name: "DENTISTRY", image: "../images/services.svg" },
-    // { name: "GASTROENTEROLOGY", image: "../images/services.svg" },
-  ];
-
-  const swiperWrapper = document.querySelector(".swiper-wrapper");
-
-  // Dynamically create slides
-  services.forEach((service) => {
-    let slide = document.createElement("div");
-    slide.classList.add("swiper-slide");
-    slide.innerHTML = `
-            <div class="service-card">
-                <img src="${service.image}" alt="${service.name}">
-                <p>${service.name}</p>
-            </div>
-        `;
-    swiperWrapper.appendChild(slide);
-  });
-
-  // Initialize Swiper
-  new Swiper(".mySwiper", {
-    slidesPerView: 1, // Default for mobile
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
     spaceBetween: 20,
+    autoplay: {
+      delay: 3000,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
     breakpoints: {
-      768: {
-        slidesPerView: 2, // Tablets
+      640: {
+        slidesPerView: 2,
       },
       1024: {
-        slidesPerView: 3, // Desktops
+        slidesPerView: 3,
       },
     },
   });
